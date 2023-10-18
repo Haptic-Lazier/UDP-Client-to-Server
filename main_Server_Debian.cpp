c7459ff535ef0306949845af334c01de18026520
Haptic_Lazier <83362171+Haptic-Lazier@users.noreply.github.com>
2023-10-18 11:05:00 -0500

    Create main_Server_Debian.cpp

---
+++ main_Server_Debian.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <cstring>      // For std::strerror()
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>     // For close()
+
+using namespace std;
+
+int main()
+{
+    // Create a socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1)
+    {
+        cerr << "Error creating socket: " << strerror(errno) << endl;
+        return 1;
+    }
+
+    // Bind the socket to a specific address and port
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(54000);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        cerr << "Error binding socket: " << strerror(errno) << endl;
+        close(sockfd);
+        return 1;
+    }
+
+    sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    char buffer[1024];
+
+    while (true)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
+        if (bytesReceived == -1)
+        {
+            cerr << "Error receiving data: " << strerror(errno) << endl;
+            continue;
+        }
+
+        char clientIp[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &clientAddr.sin_addr, clientIp, sizeof(clientIp));
+        cout << "Message received from " << clientIp << ": " << buffer << endl;
+    }
+
+    close(sockfd);
+    return 0;
+}

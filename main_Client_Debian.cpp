5920c0a651ee0485f58303390e69f7eaaaf6303f
Haptic_Lazier <83362171+Haptic-Lazier@users.noreply.github.com>
2023-11-03 15:03:53 -0500

    Update main_Client_Debian.cpp

--- main_Client_Debian.cpp
+++ main_Client_Debian.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <cstring>      // For std::strerror()
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,8 +11,8 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
-        cout << "Usage: " << argv[0] << " <message>" << endl;
+    if (argc < 4) {
+        cout << "Usage: " << argv[0] << " <server-name> <server-port> <sourc$
         return 1;
     }
 
@@ -25,26 +26,43 @@ int main(int argc, char* argv[])
     // Set up the server details
     sockaddr_in server;
     server.sin_family = AF_INET;
-    server.sin_port = htons(54000);
-    if (inet_pton(AF_INET, "127.0.0.1", &server.sin_addr) <= 0) {
+    server.sin_port = htons(stoi(argv[2]));
+    if (inet_pton(AF_INET, argv[1], &server.sin_addr) <= 0) {
         perror("inet_pton() failed");
         close(sockfd);
         return 1;
     }
 
-    // Send the message
-    string s(argv[1]);
-    ssize_t bytesSent = sendto(sockfd, s.c_str(), s.size() + 1, 0, (struct sockaddr*)&server, sizeof(server));
-    if (bytesSent == -1) {
-        perror("sendto() failed");
-        close(sockfd);
+    // Opening the file
+    ifstream in(argv[3]); // the in flag is optional
+
+    if (!in) {
+        cerr << "Failed to open" << argv[3] << endl;
         return 1;
     }
+//Send the message
+    string s;
+    while (getline(in, s)){
+        ssize_t bytesSent = sendto(sockfd, s.c_str(), s.size() + 1, 0, (stru$
+        if (bytesSent == -1) {
+                perror("sendto() failed");
+                close(sockfd);
+                return 1;
+        }
+    }
 
-    cout << "Message sent: " << s << endl;
+    string eof = "END_OF_FILE_MARKER";
+    sendto(sockfd, eof.c_str(), eof.size() + 1, 0, (struct sockaddr*)&server$
 
-    // Close the socket
+    cout << "File is sent successfully" << endl;
+    cout << "Waiting for confirmation..." << endl;
+
+    char buffer[1024];
+        memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recvfrom(sockfd, buffer, sizeof(buffer), 0, null$
+        if (bytesReceived > 0 && string(buffer) == "MSR") {
+            cout << "File recieved successfully" << endl;
+        }
     close(sockfd);
-
     return 0;
 }
